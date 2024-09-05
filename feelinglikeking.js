1b9a15514d65a0e255556d88a9fcc465593e0834
arnav-brahmapurikar <167853374+arnav-brahmapurikar@users.noreply.github.com>
2024-09-05 21:37:17 +0530

    Add files via upload

---
+++ feelinglikeking.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+function displaytoshop(lolid){
+   
+
+   location.href = "shoppage.html"
+   let b = document.getElementsByClassName[0]("shoplist");
+   b.innerhtml = lolid.getElementsByClassName[0]("itemimage").outerhtml + lolid.getElementsByClassName[0]("itemname").outerhtml + lolid.getElementsByClassName[0]("itemprice").outerhtml ;
+  
+}
